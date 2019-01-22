4778a3bf4e4b42c10cf5af29907f76d61a8a91ae
Robert Wood Jr <rdrwood@gmail.com>
2019-01-22 14:57:15 -0800

    fixed math and spelling

--- ProgAssignment3/Main.cpp
+++ ProgAssignment3/Main.cpp
@@ -14,21 +14,20 @@ int main() {
 	
 
 	int howMany = 0;
-	float awnser = 0;
+	double answer = 0;
 	
 	cout << "We will do a sum for you." << endl
 		<< "How many terms do you want? ";
 	cin >> howMany;
 	cout << endl;
 	
-	awnser = (1 / (1 + sqrt(2))); 
-	if (howMany > 1) {
-		for (int i = 2; i < howMany + 2; i++) {
-			awnser += (1 / (sqrt(i) + sqrt(i +1)));
+	if (howMany > 0) {
+		for (int i = 1; i <= howMany ; i++) {
+			answer += (1 / (sqrt(i) + sqrt(i +1)));
 		}
 	}
 
-	cout << setprecision(2) << fixed <<  awnser << endl;
+	cout << setprecision(2) << fixed << answer << endl;
 
 	system("pause");
 	return 0;
